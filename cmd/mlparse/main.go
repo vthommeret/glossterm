24a8270833ef4a75696552999dc661ad76062a88
Vernon Thommeret <vernon@signalsciences.com>
2016-12-17 10:03:16 -0800

    allow data to be piped in

--- cmd/mlparse/main.go
+++ cmd/mlparse/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -9,14 +10,24 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatalf("Must specify file.")
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		log.Fatalf("Unable to stat stdin.")
 	}
 
-	fp := os.Args[1]
-	f, err := os.Open(fp)
-	if err != nil {
-		log.Fatalf("Unable to open fp: %s", err)
+	var f io.Reader
+
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		f = os.Stdin
+	} else {
+		if len(os.Args) < 2 {
+			log.Fatalf("Must specify file.")
+		}
+		fp := os.Args[1]
+		f, err = os.Open(fp)
+		if err != nil {
+			log.Fatalf("Unable to open fp: %s", err)
+		}
 	}
 
 	b, err := ioutil.ReadAll(f)
