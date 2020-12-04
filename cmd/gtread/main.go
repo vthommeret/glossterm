01350782e9fe15963831cc86e8de3c4c6cbf2fe8
Vernon Thommeret <vernon@practice.community>
2020-12-03 22:30:11 -1000

    update gtread to read a parsed word (useful for verifying resolved descendants)

--- cmd/gtread/main.go
+++ cmd/gtread/main.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/vthommeret/glossterm/lib/gt"
 )
@@ -19,12 +21,32 @@ func init() {
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Must specify word, e.g. es/helado.")
+	}
+	w := os.Args[1]
+
+	parts := strings.Split(w, "/")
+	if len(parts) < 2 {
+		log.Fatalf("Word must be in <lang>/<word> format, e.g. es/helado")
+	}
+
+	lang := parts[0]
+	word := parts[1]
+
 	words, err := gt.GetWords(input)
 	if err != nil {
 		log.Fatal("Unable to get %q words: %s", input, err)
 	}
 
-	b, err := json.MarshalIndent(words, "", "  ")
+	if _, ok := words[word]; !ok {
+		log.Fatalf("Unable to find word: %s", word)
+	}
+	if _, ok := words[word].Languages[lang]; !ok {
+		log.Fatalf("Unable to find language %s for word: %s", lang, word)
+	}
+
+	b, err := json.MarshalIndent(words[word].Languages[lang], "", "  ")
 	if err != nil {
 		log.Fatalf("Unable to marshal JSON: %s", err)
 	}
